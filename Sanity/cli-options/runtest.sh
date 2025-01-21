e89b8359f0fd712188bf95ad78a3e4183c7c1256
Patrik Končitý <44195706+Koncpa@users.noreply.github.com>
2025-01-21 13:14:04 +0100

    Improve grep of glibc library files for new RHELs

--- Sanity/cli-options/runtest.sh
+++ Sanity/cli-options/runtest.sh
@@ -161,7 +161,11 @@ rlJournalStart && {
       rlRun "cp '$(rpm -qal | grep '\.pyc$' | head -n 1)' files/application_x-bytecode.python_.pyc"
       rlRun "cp '$(rpm -qal | grep '\.so$' | grep -v libc | grep -v pthread | head -n 1)' files/application_x-sharedlib_non-libc.so"
       rlRun "cp /bin/bash files/application_x-executable_bash"
-      libc=$(rpm -ql glibc | grep -E '^/lib(64)?/libc\b.*\.so(\.[0-9]+)?$' | head -n1)
+      if rlIsRHELLike ">=" 9.6 ; then
+        libc=$(rpm -ql glibc | grep -E '^(/usr)?/lib(64)?/libc\b.*\.so(\.[0-9]+)?$' | head -n1)
+      else
+        libc=$(rpm -ql glibc | grep -E '^/lib(64)?/libc\b.*\.so(\.[0-9]+)?$' | head -n1)
+      fi
       tcfChk "checking $libc for application/x-sharedlib" && {
         echo -n "file's output:      "; file --mime $libc
         echo -n "fapolicyd's output: "; fapolicyd-cli -t $libc | tee out
